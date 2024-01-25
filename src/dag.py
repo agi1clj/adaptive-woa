f205b26c78f2686fad55a4f9fd43a5d1be5dc591
Arcas Gabriel-Ioan <agi1clj@bosch.com>
2024-01-25 09:29:16 +0000

    feat: add initial version of multi objective function implementation problem

--- src/dag.py
+++ src/dag.py
@@ -3,75 +3,113 @@ from pyvis.network import Network
 import random
 import json
 
+
 def generate_dag():
     dag = nx.DiGraph()
 
-    aws_regions = ['eu-west-1', 'eu-west-2', 'eu-central-1', 'eu-west-3', 'eu-north-1', 'eu-south-1', 'eu-west-2']
+    aws_regions = [
+        "eu-west-1",
+        "eu-west-2",
+        "eu-central-1",
+        "eu-west-3",
+        "eu-north-1",
+        "eu-south-1",
+        "eu-west-2",
+    ]
 
     # Add edge tasks
-    edge_task_names = [f'Edge_Task_{i}' for i in range(10)]
+    edge_task_names = [f"Edge_Task_{i}" for i in range(10)]
     for task_name in edge_task_names:
-        edge_provider = random.choice(['Cluj-MdC-01', 'Sibiu-MdC-02', 'Timisoara-MdC-03'])
-        dag.add_node(task_name, type='EdgeTask', region='eu-central-1',
-                     provider=edge_provider,
-                     data_volume=random.uniform(1, 10),  # in GB
-                     computational_requirements={'CPU': random.uniform(5, 10), 'Memory': random.uniform(10, 20)},  # in GHz, GB
-                     latency=random.uniform(1, 5),  # in ms
-                     bandwidth=random.uniform(5, 10),  # in Mbps
-                     energy_consumption=random.uniform(20, 40))  # in Watts
+        edge_provider = random.choice(
+            ["Cluj-MdC-01", "Sibiu-MdC-02", "Timisoara-MdC-03"]
+        )
+        dag.add_node(
+            task_name,
+            type="EdgeTask",
+            region="eu-central-1",
+            provider=edge_provider,
+            data_volume=random.uniform(1, 10),  # in GB
+            computational_requirements={
+                "CPU": random.uniform(5, 10),
+                "Memory": random.uniform(10, 20),
+            },  # in GHz, GB
+            latency=random.uniform(1, 5),  # in ms
+            bandwidth=random.uniform(5, 10),  # in Mbps
+            energy_consumption=random.uniform(20, 40),
+        )  # in Watts
 
     # Add fog tasks
-    fog_task_names = [f'Fog_Task_{i}' for i in range(5)]
+    fog_task_names = [f"Fog_Task_{i}" for i in range(5)]
     for task_name in fog_task_names:
-        fog_provider = random.choice(['Cluj-DC-01', 'Brasov-DC-02', 'Bucuresti-DC-03'])
-        dag.add_node(task_name, type='FogTask', region='eu-central-1',
-                     provider=fog_provider,
-                     data_volume=random.uniform(10, 50),  # in GB
-                     computational_requirements={'CPU': random.uniform(10, 30), 'Memory': random.uniform(30, 50)},  # in GHz, GB
-                     latency=random.uniform(5, 10),  # in ms
-                     bandwidth=random.uniform(10, 20),  # in Mbps
-                     energy_consumption=random.uniform(30, 60))  # in Watts
+        fog_provider = random.choice(["Cluj-DC-01", "Brasov-DC-02", "Bucuresti-DC-03"])
+        dag.add_node(
+            task_name,
+            type="FogTask",
+            region="eu-central-1",
+            provider=fog_provider,
+            data_volume=random.uniform(10, 50),  # in GB
+            computational_requirements={
+                "CPU": random.uniform(10, 30),
+                "Memory": random.uniform(30, 50),
+            },  # in GHz, GB
+            latency=random.uniform(5, 10),  # in ms
+            bandwidth=random.uniform(10, 20),  # in Mbps
+            energy_consumption=random.uniform(30, 60),
+        )  # in Watts
 
     # Add cloud tasks
-    cloud_task_names = [f'Cloud_Task_{i}' for i in range(5)]
+    cloud_task_names = [f"Cloud_Task_{i}" for i in range(5)]
     for task_name in cloud_task_names:
         region = random.choice(aws_regions)
-        provider = random.choice(['AWS', 'Azure', 'Heroku', 'GCP'])
-        dag.add_node(task_name, type='CloudTask', region=region,
-                     provider=provider,
-                     data_volume=random.uniform(50, 100),  # in GB
-                     computational_requirements={'CPU': random.uniform(30, 50), 'Memory': random.uniform(50, 100)},  # in GHz, GB
-                     latency=random.uniform(10, 20),  # in ms
-                     bandwidth=random.uniform(20, 30),  # in Mbps
-                     energy_consumption=random.uniform(60, 80))  # in Watts
+        provider = random.choice(["AWS", "Azure", "Heroku", "GCP"])
+        dag.add_node(
+            task_name,
+            type="CloudTask",
+            region=region,
+            provider=provider,
+            data_volume=random.uniform(50, 100),  # in GB
+            computational_requirements={
+                "CPU": random.uniform(30, 50),
+                "Memory": random.uniform(50, 100),
+            },  # in GHz, GB
+            latency=random.uniform(10, 20),  # in ms
+            bandwidth=random.uniform(20, 30),  # in Mbps
+            energy_consumption=random.uniform(60, 80),
+        )  # in Watts
 
     # Connect tasks based on the architecture with selective links
     for edge_task in edge_task_names:
         # Connect edge tasks to a random subset of fog tasks
-        for fog_task in random.sample(fog_task_names, random.randint(1, len(fog_task_names))):
+        for fog_task in random.sample(
+            fog_task_names, random.randint(1, len(fog_task_names))
+        ):
             dag.add_edge(edge_task, fog_task)
 
     for fog_task in fog_task_names:
         # Connect fog tasks to a random subset of cloud tasks
-        for cloud_task in random.sample(cloud_task_names, random.randint(1, len(cloud_task_names))):
+        for cloud_task in random.sample(
+            cloud_task_names, random.randint(1, len(cloud_task_names))
+        ):
             dag.add_edge(fog_task, cloud_task)
 
     return dag
 
+
 # Generate the DAG
 dag = generate_dag()
-print(dag)
 
 # Set node attributes for hover text
-node_properties = {node: f"{node}, Type: {dag.nodes[node]['type']}, region: {dag.nodes[node]['region']}, "
-                              f"Provider: {dag.nodes[node]['provider']}, "
-                              f"Data Volume: {dag.nodes[node]['data_volume']:.2f} GB, "
-                              f"CPU: {dag.nodes[node]['computational_requirements']['CPU']:.2f} GHz, "
-                              f"Memory: {dag.nodes[node]['computational_requirements']['Memory']:.2f} GB, "
-                              f"Latency: {dag.nodes[node]['latency']:.2f} ms, "
-                              f"Bandwidth: {dag.nodes[node]['bandwidth']:.2f} Mbps, "
-                              f"Energy Consumption: {dag.nodes[node]['energy_consumption']:.2f} Watts"
-                   for node in dag.nodes}
+node_properties = {
+    node: f"{node}, Type: {dag.nodes[node]['type']}, Region: {dag.nodes[node]['region']}, "
+    f"Provider: {dag.nodes[node]['provider']}, "
+    f"Data Volume: {dag.nodes[node]['data_volume']:.2f} GB, "
+    f"CPU: {dag.nodes[node]['computational_requirements']['CPU']:.2f} GHz, "
+    f"Memory: {dag.nodes[node]['computational_requirements']['Memory']:.2f} GB, "
+    f"Latency: {dag.nodes[node]['latency']:.2f} ms, "
+    f"Bandwidth: {dag.nodes[node]['bandwidth']:.2f} Mbps, "
+    f"Energy Consumption: {dag.nodes[node]['energy_consumption']:.2f} Watts"
+    for node in dag.nodes
+}
 
 # Create Pyvis network
 g = Network(notebook=True)
@@ -81,8 +119,14 @@ g.force_atlas_2based()
 
 # Add nodes and edges
 for node, properties in node_properties.items():
-    node_type = dag.nodes[node]['type']
-    color = 'green' if node_type == 'EdgeTask' else 'blue' if node_type == 'FogTask' else 'purple'
+    node_type = dag.nodes[node]["type"]
+    color = (
+        "green"
+        if node_type == "EdgeTask"
+        else "blue"
+        if node_type == "FogTask"
+        else "purple"
+    )
     g.add_node(node, title=properties, color=color)
 
 for edge in dag.edges:
@@ -93,5 +137,5 @@ g.show("static/dag.html")
 dag_json = nx.node_link_data(dag)
 
 # Save the JSON to a file
-with open('static/dag_model.json', 'w') as json_file:
+with open("static/dag_model.json", "w") as json_file:
     json.dump(dag_json, json_file)
--- src/woa_task_shifting.py
+++ src/woa_task_shifting.py
@@ -1,65 +1,19 @@
 import numpy as np
 from mealpy import PermutationVar, WOA, Problem
 import json
+import logging
 
-def objective_min_energy_consumption(problem, tasks):
-    """
-    Objective 1: Minimize energy consumption.
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s, %(levelname)s, %(name)s: %(message)s",
+    datefmt="%Y/%m/%d %I:%M:%S %p",
+)
+logger = logging.getLogger(__name__)
 
-    This objective aims to minimize the total energy consumption of the selected tasks.
-
-    Parameters:
-        problem (TaskShiftingProblem): The optimization problem instance.
-        tasks (list): List of task indices representing the assignment order.
-
-    Returns:
-        float: Total energy consumption for the selected tasks.
-    """
-    energy_consumption = sum(problem.data["nodes"][task]["energy_consumption"] for task in tasks)
-    return energy_consumption
-
-
-def objective_min_latency(problem, tasks):
-    """
-    Objective: Minimize total latency.
-
-    This objective aims to find the optimal deployment location based on minimizing the total latency of the selected tasks.
-
-    Parameters:
-        problem (TaskShiftingProblem): The optimization problem instance.
-        tasks (list): List of task indices representing the assignment order.
-
-    Returns:
-        float: Optimal deployment location based on minimizing latency.
-    """
-    latencies = [problem.data["nodes"][task]["latency"] for task in tasks]
-    total_latency = sum(latencies)
-    optimal_location = total_latency / len(tasks)  # Simple example, can be more complex
-    print(optimal_location)
-    return optimal_location
-
-def objective_find_deployment_location_data_volume(problem, tasks):
-    """
-    Objective: Find optimal deployment location based on data volume.
-
-    This objective aims to find the optimal deployment location based on the total data volume of the selected tasks.
-
-    Parameters:
-        problem (TaskShiftingProblem): The optimization problem instance.
-        tasks (list): List of task indices representing the assignment order.
-
-    Returns:
-        float: Optimal deployment location based on data volume.
-    """
-    data_volumes = [problem.data["nodes"][task]["data_volume"] for task in tasks]
-    total_data_volume = sum(data_volumes)
-    optimal_location = total_data_volume / len(tasks)  # Simple example, can be more complex
-    print(optimal_location)
-
-    return optimal_location
 
 class TaskShiftingProblem(Problem):
-    def __init__(self, bounds=None, minmax="min", data=None, objectives=None, weights=None, **kwargs):
+    def __init__(self, bounds=None, minmax="min", data=None, name="TaskShifting", **kwargs):
         """
         Task Shifting Multi-objective Optimization Problem.
 
@@ -67,61 +21,125 @@ class TaskShiftingProblem(Problem):
             bounds (PermutationVar): Permutation bounds for the task assignment order.
             minmax (str): "min" for minimization problem.
             data (dict): Problem data containing node and link information.
-            objectives (list): List of objective functions to be minimized.
-            weights (list): List of weights corresponding to each objective function.
+            obj_weights (list): List of weights corresponding to each objective function.
+            name (str): Name of the problem.
             **kwargs: Additional keyword arguments for the superclass constructor.
         """
         self.data = data
-        self.objectives = objectives
-        self.weights = weights
-        super().__init__(bounds, minmax, **kwargs)
+        super().__init__(bounds, minmax, name=name, **kwargs)
 
-    def obj_func(self, x):
+    def objective_min_energy_consumption(self, tasks):
+        """
+        Objective 1: Minimize energy consumption.
+
+        This objective aims to minimize the total energy consumption of the selected tasks.
+
+        Parameters:
+            tasks (list): List of task indices representing the assignment order.
+
+        Returns:
+            float: Total energy consumption for the selected tasks.
+        """
+        tasks_list = tasks.astype(int).tolist()
+        energy_consumption = sum(self.data["nodes"][task]["energy_consumption"] for task in tasks_list)
+        return energy_consumption
+
+    def objective_min_latency(self, tasks):
+        """
+        Objective: Minimize total latency.
+
+        This objective aims to find the optimal deployment location based on minimizing the total latency of the selected tasks.
+
+        Parameters:
+            tasks (list): List of task indices representing the assignment order.
+
+        Returns:
+            float: Optimal deployment location based on minimizing latency.
+        """
+        tasks_list = tasks.astype(int).tolist()
+        latencies = [self.data["nodes"][task]["latency"] for task in tasks_list]
+        total_latency = sum(latencies)
+        optimal_location = total_latency / len(tasks)  # Simple example, can be more complex
+        return optimal_location
+
+    def objective_find_deployment_location_data_volume(self, tasks):
+        """
+        Objective: Find optimal deployment location based on data volume.
+
+        This objective aims to find the optimal deployment location based on the total data volume of the selected tasks.
+
+        Parameters:
+            tasks (list): List of task indices representing the assignment order.
+
+        Returns:
+            float: Optimal deployment location based on data volume.
+        """
+        tasks_list = tasks.astype(int).tolist()
+        data_volumes = [self.data["nodes"][task]["data_volume"] for task in tasks_list]
+        total_data_volume = sum(data_volumes)
+        optimal_location = total_data_volume / len(tasks)  # Simple example, can be more complex
+        return optimal_location
+
+    def obj_func(self, tasks):
         """
         Multi-objective function to be minimized.
 
         Parameters:
-            x (numpy.ndarray): Decoded solution containing the assignment order of tasks.
+            tasks (numpy.ndarray): Decoded solution containing the assignment order of tasks.
 
         Returns:
-            float: Weighted sum of objective function values.
+            list: Objective function values.
         """
-        x_decoded = self.decode_solution(x)
-        tasks = x_decoded["per_var"]
-
-        objectives_values = [objective(self, tasks) for objective in self.objectives]
-        fitness = np.dot(self.weights, objectives_values)
-
-        return fitness
+        return [
+            self.objective_min_energy_consumption(tasks),
+            self.objective_min_latency(tasks),
+            self.objective_find_deployment_location_data_volume(tasks),
+        ]
 
 
-with open('static/dag_model.json', 'r') as file:
-    task_shifting_data = json.load(file)
+def load_task_shifting_data(file_path):
+    """Load task shifting data from a JSON file."""
+    with open(file_path, "r") as file:
+        return json.load(file)
 
-# Define objectives and their weights
-objectives = [objective_min_energy_consumption, objective_min_latency, objective_find_deployment_location_data_volume]
-weights = [0.4, 0.1, 0.4]
 
-# Extract necessary information from the loaded data
-n_tasks = len(task_shifting_data["nodes"])
+def main():
+    # Load task shifting data
+    task_shifting_data = load_task_shifting_data("static/dag_model.json")
 
-'''
-The permutation represents a task sequence, 
-and the optimization algorithm determines the 
-best assignment order to computing nodes (Edge, Fog, Cloud) 
-based on defined objectives and constraints.
-'''
-task_bounds = PermutationVar(valid_set=list(range(n_tasks)), name="per_var")
+    # Extract necessary information from the loaded data
+    n_tasks = len(task_shifting_data["nodes"])
 
-# Create the TaskShiftingProblem instance
-task_shifting_problem = TaskShiftingProblem(bounds=task_bounds, minmax="min", data=task_shifting_data,
-                                             objectives=objectives, weights=weights)
+    # Define permutation bounds
+    task_bounds = PermutationVar(valid_set=list(range(n_tasks)), name="per_var")
 
-# Solve the problem using WOA algorithm
-model = WOA.OriginalWOA(epoch=100, pop_size=20)
-model.solve(task_shifting_problem)
+    # Define objectives weights
+    weights = [0.4, 0.1, 0.5]
 
-# Print results
-best_solution = task_shifting_problem.decode_solution(model.g_best.solution)
-print(f"Best solution: {best_solution}")
-print(f"Best fitness: {model.g_best.target.fitness}")
+    # Create the TaskShiftingProblem instance
+    task_shifting_problem_multi = TaskShiftingProblem(
+        bounds=task_bounds, minmax="min", data=task_shifting_data, obj_weights=weights
+    )
+
+    # Solve the problem using WOA algorithm
+    model = WOA.OriginalWOA(epoch=1000, pop_size=20)
+    model.solve(problem=task_shifting_problem_multi)
+
+    # Visualization purposes
+    # You can access them all via object "history" like this:
+    model.history.save_global_objectives_chart(filename="static/algo/goc")
+    model.history.save_local_objectives_chart(filename="static/algo/loc")
+    model.history.save_global_best_fitness_chart(filename="static/algo/gbfc")
+    model.history.save_local_best_fitness_chart(filename="static/algo/lbfc")
+    model.history.save_runtime_chart(filename="static/algo/rtc")
+    model.history.save_exploration_exploitation_chart(filename="static/algo/eec")
+    model.history.save_diversity_chart(filename="static/algo/dc")
+
+    # Print results
+    best_solution = task_shifting_problem_multi.decode_solution(model.g_best.solution)
+    logger.info(f"Best solution: {best_solution}")
+    logger.info(f"Best fitness: {model.g_best.target.fitness}")
+
+
+if __name__ == "__main__":
+    main()
