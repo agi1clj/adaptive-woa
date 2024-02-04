c0887d2d694099fa5134b37c441fd82fe8029ee3
Arcas Gabriel-Ioan <agi1clj@bosch.com>
2024-02-04 15:52:37 +0000

    feat(woa): add euclidean distance for mem, cpu, ram

--- src/dag.py
+++ src/dag.py
@@ -3,21 +3,22 @@ from pyvis.network import Network
 import random
 import json
 
-def generate_tasks(task_names, task_type, regions, providers, data_volume_range, computational_requirements_range, energy_consumption_range):
+def generate_tasks(task_names, task_type, regions, providers, cpu_ghz_range, memory_gb_range):
     tasks = {}
+    ram_values = [2, 4, 8, 16, 32]
     for task_name in task_names:
         region = random.choice(regions)
         provider = random.choice(providers)
+        is_cloud_task = task_type == "CloudTask"
         tasks[task_name] = {
             "type": task_type,
             "region": region,
             "provider": provider,
-            "data_volume": random.uniform(*data_volume_range),
-            "computational_requirements": {
-                "cpu": random.uniform(*computational_requirements_range),
-                "memory": random.uniform(*computational_requirements_range),
+            "computational_requirements" if is_cloud_task else "computational_capacity": {
+                "cpu_ghz": random.uniform(*cpu_ghz_range),
+                "memory_gb": random.uniform(*memory_gb_range),
+                "ram_gb": random.choice(ram_values)
             },
-            "energy_consumption": random.uniform(*energy_consumption_range),
         }
     return tasks
 
@@ -33,16 +34,16 @@ def generate_edges(source_tasks, target_tasks, edge_func):
 def edge_func_latency_bandwidth(latency_range, bandwidth_range, distance_range):
     def inner(source, target):
         return (source, target, {
-            "latency": random.uniform(*latency_range),
-            "bandwidth": random.uniform(*bandwidth_range),
-            "distance": random.uniform(*distance_range)
+            "latency_ms": random.uniform(*latency_range),
+            "bandwidth_mbps": random.uniform(*bandwidth_range),
+            "distance_km": random.uniform(*distance_range)
         })
     return inner
 
-def generate_dag(fog_layer=True):
+def generate_dag():
     dag = nx.DiGraph()
 
-    aws_regions = [
+    regions = [
         "eu-west-1",
         "eu-west-2",
         "eu-central-1",
@@ -52,82 +53,65 @@ def generate_dag(fog_layer=True):
         "eu-west-2",
     ]
 
-    edge_task_names = [f"Edge_Task_{i}" for i in range(35)]
-    fog_task_names = [f"Fog_Task_{i}" for i in range(5)] if fog_layer else []
-    cloud_task_names = [f"Cloud_Task_{i}" for i in range(100)]
+    edge_task_names = [f"Edge_Task_{i}" for i in range(5)]
+    cloud_task_names = [f"Cloud_Task_{i}" for i in range(12)]
 
     edge_tasks = generate_tasks(
-        edge_task_names,
-        "EdgeTask",
-        aws_regions,
-        ["MdC-01", "MdC-02", "MdC-03"],
-        (1, 10),
-        (5, 10),
-        (20, 40)
+        task_names=edge_task_names,
+        task_type="EdgeTask",
+        regions=regions,
+        providers=["MdC-01", "MdC-02", "MdC-03"],
+        cpu_ghz_range=(30, 50),
+        memory_gb_range=(8, 32),
     )
-
-    fog_tasks = generate_tasks(
-        fog_task_names,
-        "FogTask",
-        aws_regions,
-        ["DC-01", "DC-02", "DC-03"],
-        (10, 50),
-        (10, 30),
-        (30, 60)
-    ) if fog_layer else {}
 
     cloud_tasks = generate_tasks(
-        cloud_task_names,
-        "CloudTask",
-        aws_regions,
-        ["AWS", "Azure", "Heroku", "GCP"],
-        (50, 100),
-        (30, 50),
-        (60, 80)
+        task_names=cloud_task_names,
+        task_type="CloudTask",
+        regions=regions,
+        providers=["AWS", "Azure", "Heroku", "GCP"],
+        cpu_ghz_range=(30, 50),
+        memory_gb_range=(8, 32)
     )
 
-    dag.add_nodes_from(list(edge_tasks.items()) + list(fog_tasks.items()) + list(cloud_tasks.items()))
-
-    if fog_layer:
-        edge_func_edge_to_fog = edge_func_latency_bandwidth((1, 5), (5, 10), (1, 100))
-        edge_edges = generate_edges(edge_tasks, fog_tasks, edge_func_edge_to_fog)
-        dag.add_edges_from(edge_edges)
-
-        edge_func_fog_to_cloud = edge_func_latency_bandwidth((5, 10), (10, 20), (100, 150))
-        fog_edges = generate_edges(fog_tasks, cloud_tasks, edge_func_fog_to_cloud)
-        dag.add_edges_from(fog_edges)
-    else:
-        edge_func_edge_to_cloud = edge_func_latency_bandwidth((50, 100), (1, 10), (500, 1000))
-        edge_edges = generate_edges(cloud_tasks, edge_tasks, edge_func_edge_to_cloud)
-        dag.add_edges_from(edge_edges)
+    dag.add_nodes_from(list(edge_tasks.items()) + list(cloud_tasks.items()))
+    edge_func_edge_to_cloud = edge_func_latency_bandwidth((50, 100), (1, 10), (500, 1000))
+    edge_edges = generate_edges(cloud_tasks, edge_tasks, edge_func_edge_to_cloud)
+    dag.add_edges_from(edge_edges)
 
     return dag
 
 # Generate the DAG
-dag = generate_dag(fog_layer=False)
+dag = generate_dag()
 
 # Set node attributes for hover text
 node_properties = {
-    node: f"{node}, Type: {dag.nodes[node]['type']}, Region: {dag.nodes[node]['region']}, "
-    f"Provider: {dag.nodes[node]['provider']}, "
-    f"Data Volume: {dag.nodes[node]['data_volume']:.2f} GB, "
-    f"CPU: {dag.nodes[node]['computational_requirements']['cpu']:.2f} GHz, "
-    f"Memory: {dag.nodes[node]['computational_requirements']['memory']:.2f} GB, "
-    f"Energy Consumption: {dag.nodes[node]['energy_consumption']:.2f} Watts"
+    node: (
+        f"{node}, Type: {dag.nodes[node]['type']}, Region: {dag.nodes[node]['region']}, "
+        f"Provider: {dag.nodes[node]['provider']}, "
+        f"CPU: {dag.nodes[node]['computational_requirements']['cpu_ghz']:.2f} GHz, "
+        f"Memory: {dag.nodes[node]['computational_requirements']['memory_gb']:.2f} GB, "
+        f"Ram: {dag.nodes[node]['computational_requirements']['ram_gb']:.2f} GB"
+    )
+    if dag.nodes[node]['type'] == 'CloudTask'
+    else (
+        f"{node}, Type: {dag.nodes[node]['type']}, Region: {dag.nodes[node]['region']}, "
+        f"Provider: {dag.nodes[node]['provider']}, "
+        f"CPU: {dag.nodes[node]['computational_capacity']['cpu_ghz']:.2f} GHz, "
+        f"Memory: {dag.nodes[node]['computational_capacity']['memory_gb']:.2f} GB, "
+        f"Ram: {dag.nodes[node]['computational_capacity']['ram_gb']:.2f} GB"
+    )
     for node in dag.nodes
 }
 
 edge_properties = {
-    (edge[0], edge[1]): f"Latency: {dag.edges[edge]['latency']:.2f} ms, "
-                       f"Bandwidth: {dag.edges[edge]['bandwidth']:.2f} Mbps, "
-                       f"Distance: {dag.edges[edge]['distance']:.2f} km"
+    (edge[0], edge[1]): f"Latency: {dag.edges[edge]['latency_ms']:.2f} ms, "
+                       f"Bandwidth: {dag.edges[edge]['bandwidth_mbps']:.2f} Mbps, "
+                       f"Distance: {dag.edges[edge]['distance_km']:.2f} km"
     for edge in dag.edges
 }
 
-# Create Pyvis network
 g = Network(notebook=True)
-
-# Set physics options to avoid overlap
 g.force_atlas_2based()
 
 # Add nodes and edges
@@ -137,8 +121,6 @@ for node, properties in node_properties.items():
         "green"
         if node_type == "EdgeTask"
         else "blue"
-        if node_type == "FogTask"
-        else "purple"
     )
     g.add_node(node, title=properties, color=color)
 
@@ -152,4 +134,4 @@ dag_json = nx.node_link_data(dag)
 
 # Save the JSON to a file
 with open("docs/dag_model.json", "w") as json_file:
-    json.dump(dag_json, json_file)
+    json.dump(dag_json, json_file, indent=2)
--- src/woa_task_shifting.py
+++ src/woa_task_shifting.py
@@ -1,19 +1,35 @@
+from collections import defaultdict
+import numpy as np
 from mealpy import BinaryVar, WOA, Problem
 import json
-import sys
 
-
-class EdgeCloudOffloadingProblem(Problem):
+class CloudToEdgeOffloadingProblem(Problem):
     def __init__(self, nodes):
-        self.nodes = self.extract_cloud_tasks(nodes)
+        # DAG Graph data
+        self.nodes = nodes["nodes"]
+        self.cloud_nodes = self.extract_cloud_tasks(nodes)
+        self.edge_nodes = self.extract_edge_tasks(nodes)
         self.links = nodes["links"]
-        self.eps = -10000000 # Penalty value
-        self.cloud_nodes_rtt = {node["id"]: 0 for node in self.nodes}  # Dictionary to store RTT values per cloud node
-        bounds = BinaryVar(n_vars=len(self.nodes), name="offloading_var")
+        self.cloud_nodes_results = defaultdict(lambda: {"links": defaultdict(dict), "average_rtt": 0, "average_euclidean_distance": 0})
+
+        # Penalties
+        self.eps = -1e10
+        self.threshold_penalty_factor = -10
+
+        # Thresholds
+        self.threshold_rtt = 100
+        self.threshold_euclidean_distance = 10
+
+        bounds = BinaryVar(n_vars=len(self.cloud_nodes), name="cloud_offloading_var")
         super().__init__(bounds=bounds, minmax="max", save_population=True)
 
-    def extract_cloud_tasks(self, nodes_data):
+    @staticmethod
+    def extract_cloud_tasks(nodes_data):
         return [node for node in nodes_data.get("nodes", []) if node.get("type") == "CloudTask"]
+
+    @staticmethod
+    def extract_edge_tasks(nodes_data):
+        return [node for node in nodes_data.get("nodes", []) if node.get("type") == "EdgeTask"]
 
     @staticmethod
     def calculate_rtt(bandwidth_mbps, latency_ms, distance_km):
@@ -24,43 +40,94 @@ class EdgeCloudOffloadingProblem(Problem):
         total_delay_ms = (propagation_delay + transmission_delay + latency_ms / 1000) * 1000
         return total_delay_ms
 
+    @staticmethod
+    def calculate_euclidean_distance(cloud_task, edge_task):
+        cpu_distance = abs(cloud_task["computational_requirements"]["cpu_ghz"] - edge_task["computational_capacity"]["cpu_ghz"])
+        mem_distance = abs(cloud_task["computational_requirements"]["memory_gb"] - edge_task["computational_capacity"]["memory_gb"])
+        ram_distance = abs(cloud_task["computational_requirements"]["ram_gb"] - edge_task["computational_capacity"]["ram_gb"])
+        return np.sqrt(cpu_distance**2 + mem_distance**2 + ram_distance**2)
+
     def obj_func(self, x):
         x_decoded = self.decode_solution(x)
-        offloading_var = x_decoded["offloading_var"]
+        offloading_var = x_decoded["cloud_offloading_var"]
         total_rtt = 0  # Initialize the maximum round-trip time
-        for i, source_node in enumerate(self.nodes):
+        total_distance = 0  # Initialize the total distance penalty
+        for i, cloud_node in enumerate(self.cloud_nodes):
             if offloading_var[i] == 1:
-                relevant_links = [link for link in self.links if link["source"] == source_node["id"]]
+                relevant_links = [link for link in self.links if link["source"] == cloud_node["id"]]
+                relevant_links_rtt = 0
+                relevant_links_distance = 0
+
                 for link in relevant_links:
-                    total_rtt += self.calculate_rtt(link["bandwidth"], link["latency"], link["distance"])
-                average_rtt = total_rtt / len(relevant_links)
-                total_rtt = average_rtt
-                self.cloud_nodes_rtt[source_node["id"]] = total_rtt
+                    link_rtt = self.calculate_rtt(link["bandwidth_mbps"], link["latency_ms"], link["distance_km"])
+                    relevant_links_rtt += link_rtt
+
+                    target_edge_node = [edge_node for edge_node in self.edge_nodes if edge_node['id'] == link['target']][0]
+                    link_distance = self.calculate_euclidean_distance(cloud_node, target_edge_node)
+                    relevant_links_distance  += link_distance
+                    self.cloud_nodes_results[cloud_node["id"]]["links"][target_edge_node["id"]] = {
+                        'euclidean_distance': link_distance,
+                        'rtt': link_rtt
+                        }
+                    
+                    # Penalty for eucledean distance
+                    if link_distance > self.threshold_euclidean_distance:
+                        total_distance += self.threshold_penalty_factor * (link_distance - self.threshold_euclidean_distance)
+
+                    # Penalty for RTT
+                    if link_rtt > self.threshold_rtt:
+                        total_rtt += self.threshold_penalty_factor * (link_rtt - self.threshold_rtt)
+
+
+                average_relevant_links_rtt = relevant_links_rtt / len(relevant_links)
+                average_relevant_link_distance = relevant_links_distance / len(relevant_links)
+
+                total_rtt += average_relevant_links_rtt
+                total_distance += average_relevant_link_distance
+
+
+                self.cloud_nodes_results[cloud_node["id"]]["total_rtt"] = relevant_links_rtt
+                self.cloud_nodes_results[cloud_node["id"]]["total_euclidean_distance"] = relevant_links_distance
+                self.cloud_nodes_results[cloud_node["id"]]["average_rtt"] = average_relevant_links_rtt
+                self.cloud_nodes_results[cloud_node["id"]]["average_euclidean_distance"] = average_relevant_link_distance
+                
 
         if total_rtt == 0:  # If any cloud node has 0 value, it indicates that this node is not having any edge.
             return self.eps
 
-        # A good round-trip time (RTT) should be below 100 milliseconds for optimal performance.
-        # https://aws.amazon.com/what-is/rtt-in-networking/
-        if total_rtt > 100:
-             return self.eps + total_rtt
-           
-        return total_rtt
+        return total_rtt + total_distance
 
 
 if __name__ == "__main__":
-    sys.stdout = open('docs/output.txt', 'w')
-    sys.stdout = sys.__stdout__
-    # Example usage for cloud offloading to edge
+    # Load the input DAG graph for cloud to edge offloading
     with open("docs/dag_model.json", "r") as file:
         nodes_data = json.load(file)
 
-    problem = EdgeCloudOffloadingProblem(nodes_data)
-    model = WOA.HI_WOA(epoch=1000, pop_size=5)
+    problem = CloudToEdgeOffloadingProblem(nodes_data)
+    model = WOA.HI_WOA(epoch=1000, pop_size=20)
     model.solve(problem)
 
-    # Visualization purposes
-    # You can access them all via object "history" like this:
+    # Output results from the problem into JSON
+    cloud_offloading_var = problem.decode_solution(model.g_best.solution)['cloud_offloading_var']
+    cloud_tasks_to_shift = [cloud_node['id'] for i, cloud_node in enumerate(problem.cloud_nodes) if cloud_offloading_var[i] == 1]
+    output_data = {
+    "offloading_decision": {
+        "cloud_tasks_total_number": len(problem.nodes),
+        "cloud_tasks_total_number_to_shift": len(cloud_tasks_to_shift),
+        "cloud_tasks_to_shift_from_cloud_to_edge": cloud_tasks_to_shift
+    },
+    "offloading_debug": problem.cloud_nodes_results
+    }
+    
+    with open('docs/cloud_offloading_results.json', 'w') as results_file:
+        json.dump(output_data, results_file, indent=2)
+
+    print(f"Best agent: {model.g_best}")
+    print(f"Best solution: {model.g_best.solution}")
+    print(f"Best fitness: {model.g_best.target.fitness}")
+    print(f"Best parameters: {model.problem.decode_solution(model.g_best.solution)}")
+
+    # Algorithm charts Visualization
     charts_path = "docs/algo/"
     model.history.save_global_objectives_chart(filename=f"{charts_path}goc")
     model.history.save_local_objectives_chart(filename=f"{charts_path}loc")
@@ -70,24 +137,3 @@ if __name__ == "__main__":
     model.history.save_exploration_exploitation_chart(filename=f"{charts_path}eec")
     model.history.save_diversity_chart(filename=f"{charts_path}dc")
     model.history.save_trajectory_chart(filename=f"{charts_path}tc")
-
-    # Save prints into a file
-    with open('docs/output.txt', 'w') as output_file:
-        print("\n------------------- Algorithm output ---", file=output_file)
-        print(f"Best agent: {model.g_best}", file=output_file)
-        print(f"Best solution: {model.g_best.solution}", file=output_file)
-        print(f"Best fitness: {model.g_best.target.fitness}", file=output_file)
-        print(f"Best offloading configuration: {problem.decode_solution(model.g_best.solution)}", file=output_file)
-
-        offloading_var = problem.decode_solution(model.g_best.solution)['offloading_var']
-
-        cloud_tasks_to_shift = [node_id for i, node_id in enumerate(problem.cloud_nodes_rtt) if offloading_var[i] == 1]
-        print("\n-------------------- Offloading decision ---", file=output_file)
-        print(f"CloudTasks total number: {len(problem.nodes)}", file=output_file)
-        print(f"CloudTasks total number to shift: {len(cloud_tasks_to_shift)}", file=output_file)
-        print(f"CloudTasks to shift from cloud to edge: {cloud_tasks_to_shift}", file=output_file)
-        print("\n-------------------- Offloading Debug --- ", file=output_file)
-        # Display RTT values for the tasks shifted
-        rtt_values_for_shifted_tasks = {node_id: problem.cloud_nodes_rtt[node_id] for node_id in cloud_tasks_to_shift}
-        print("RTT values for selected Cloud tasks:", rtt_values_for_shifted_tasks, file=output_file)
-        print("All RTT values for all Cloud tasks: ", problem.cloud_nodes_rtt, file=output_file)
